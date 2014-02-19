af3b583f7f03fd390d31b3dfcd8718602840e8b6
fibo <casati_gianluca@yahoo.it>
2014-02-19 16:03:11 +0100

    gulpified

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 
-require('strict-mode')(function () {
-
-  exports.IperElement = require('./classes/IperElement.js')
-  exports.IperEdge    = require('./classes/IperEdge.js')
-  exports.IperGraph   = require('./classes/IperGraph.js')
-  exports.IperNode    = require('./classes/IperNode.js')
-
-})
+module.exports = require('./src/index')
 
---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+require('strict-mode')(function () {
+
+  exports.IperElement = require('./IperElement.js')
+  exports.IperEdge    = require('./IperEdge.js')
+  exports.IperGraph   = require('./IperGraph.js')
+  exports.IperNode    = require('./IperNode.js')
+
+})
+
